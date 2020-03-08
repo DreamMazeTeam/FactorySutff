a569ce7e2e911b01bb42d6d438facfa658969b84
DreamMazeTeam <58484975+DreamMazeTeam@users.noreply.github.com>
2020-03-08 13:31:38 +0300

    StartUp

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,37 @@
-#include <Arduino.h>
+// Если код собирается в ардуиона ide
+//#define ARDUINO_IDE
 
-void setup() {
-  // put your setup code here, to run once:
+#ifndef ARDUINO_IDE
+#include <Arduino.h>
+#endif
+
+// Для отладочной информации
+//#define DEBUG 
+#ifdef DEBUG
+    #define print(x) Serial.print(x)
+    #define println(x) Serial.println(x)
+#else
+    #define print(x)
+    #define println(x)
+#endif
+
+#include "liquid_crystal_i2c.h"
+#include "nrf24l01.h"
+#include "encoder.h"
+#include "rf24.h"
+
+RF24* radio;
+Encoder* encoder;
+LiquidCrystal* lcd;
+
+void setup()
+{
+#ifdef DUBUG
+    Serial.begin(9600);
+#endif
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    
 }
