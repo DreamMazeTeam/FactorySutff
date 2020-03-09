95fe963c74a3895d097f7bbaa5b37bcc4226b84a
DreamMazeTeam <58484975+DreamMazeTeam@users.noreply.github.com>
2020-03-09 17:33:21 +0300

    s

--- src/main.cpp
+++ src/main.cpp
@@ -2,30 +2,10 @@
 #include <Arduino.h>
 #include <SPI.h>
 
-#pragma region DEBUG
-
-#ifdef DEBUG
-
-#define print(x) Serial.print(x)
-#define println(x) Serial.println(x)
-
-#else
-
-#define print(x)
-#define println(x)
-
-#endif
-
-#pragma endregion
-#pragma region INCLUDES
-
 #include "liquid_crystal_i2c.h"
 #include "nrf24l01.h"
 #include "encoder.h"
 #include "rf24.h"
-
-#pragma endregion
-#pragma region PINPUT
 
 #define pinCeRf24 0
 #define pinCsRf24 0
@@ -33,21 +13,18 @@
 #define pinEncClk 0
 #define pinEncDt  0
 
-#pragma endregion
+#define SLAVE  0
+#define MASTER 1
 
-RF24* radio;
-Encoder* encoder;
-LiquidCrystal* lcd;
+#define MODE SLAVE
+
+RF24 radio(pinCeRf24, pinCsRf24);
+Encoder encoder(pinEncClk, pinEncDt, pinEncSw);
+LiquidCrystal lcd(0x68, 12, 2);
 
 void setup()
 {
-#ifdef DUBUG
-    Serial.begin(9600);
-#endif
 
-    radio = new RF24(pinCeRf24, pinCsRf24);
-    encoder = new Encoder(pinEncClk, pinEncDt, pinEncSw);
-    lcd = new LiquidCrystal(0x68, 12, 2);
 }
 
 void loop() {
